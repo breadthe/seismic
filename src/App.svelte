d7f8b13a72738bea6438d79f4eb94790f725b52c
breadthe <breadthe@users.noreply.github.com>
2023-02-07 09:07:33 -1000

    Styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,75 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import type { FeatureCollection } from "./types"
+
+  const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
+  let downloadError = ""
+  let loading = false
+  let quakeData: FeatureCollection | undefined = undefined
+
+  async function refreshData() {
+    loading = true
+    await innerDownload()
+    loading = false
+  }
+
+  async function innerDownload() {
+    downloadError = ""
+    try {
+      const response = await fetch(url)
+
+      quakeData = await response.json()
+      console.log(quakeData)
+
+      if (response.status !== 200) {
+        downloadError = "Could not download"
+        console.error("Error response from USGS", quakeData)
+        return
+      }
+    } catch (error) {
+      downloadError = "Network error"
+      console.error(error)
+    }
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class="w-full h-full">
+  <div class="flex items-center gap-4 p-2">
+    <button
+      on:click={refreshData}
+      class="text-xl font-extrabold bg-gradient-to-br from-blue-600 to-indigo-800 hover:opacity-90 bg-clip-text text-transparent"
+    >
+      Seismic
+    </button>
+
+    {#if loading}
+      <small class="font-xs text-gray-600">fetching data...</small>
+    {/if}
+
+    {#if downloadError}
+      <small class="font-xs text-red-600">{downloadError}</small>
+    {/if}
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {#if quakeData && quakeData.type === "FeatureCollection"}
+    <div class="w-full bg-white">
+      {#each quakeData.features as quake}
+        <div class="flex items-center gap-4 p-2 border-b hover:bg-gray-100">
+          <div>
+            <span class:font-bold={quake.properties.mag >= 4.5} class:text-red-600={quake.properties.mag >= 7}>
+              {quake.properties.mag}
+            </span>
+          </div>
+          <div>
+            <p class="font-bold" class:text-red-600={quake.properties.mag >= 7}>
+              {quake.properties.place}
+            </p>
+            <small class="font-xs text-gray-600">{new Date(quake.properties.time).toLocaleString()}</small>
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
